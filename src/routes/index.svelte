72c9d7283841d1a8a34141f3dd6e33ec7b969be4
dylan99913 <dylan_wee@live.com>
2021-10-24 10:47:51 +0000

    more

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,18 @@
 <script>
 	import Header from '$lib/Header.svelte';
+    import Section from '$lib/Section.svelte';
+    let myInfo = {
+        title: "Info",
+        item1: "Dylan",
+        item2: 16,
+        item3: "not telling",
+    }
 </script>
  
 <Header title = "My Biography">
-    esfsfesf
+    
 </Header>
+
+<Section {...myInfo} >
+    
+</Section>
