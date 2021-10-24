5335030859a1946f512b2cc70bee092a74589c0b
dylan99913 <dylan_wee@live.com>
2021-10-24 10:55:06 +0000

    hobby

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,20 @@
         item2: 16,
         item3: "not telling",
     }
+    let myHobby = {
+        title: "Hobby",
+        item1: "playing games",
+        item2: "watching shows",
+        item3: "nothing",
+    }
+    let myPosonality = {
+        title: "Posonality",
+        item1: "calm",
+        item2: "patient",
+        item3: "hard working",
+    }
+
+
 </script>
  
 <Header title = "My Biography">
@@ -14,4 +28,8 @@
 </Header>
 
 <Section {...myInfo} />
+
+<Section {...myHobby} />
+
+<Section {...myPosonality} />
     
